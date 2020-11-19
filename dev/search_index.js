ab4c10cf1d227b1ff91447c93d7713ffe266cf57
Documenter.jl <documenter@juliadocs.github.io>
2020-11-19 11:59:59 +0000

    build based on c624a81

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JuTeX","category":"page"},{"location":"#JuTeX","page":"Home","title":"JuTeX","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JuTeX]","category":"page"}]
+}
